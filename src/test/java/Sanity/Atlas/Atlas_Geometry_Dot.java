b1530eca510464ef8da205c343970575b01acd58
Gayathrigayu <36566787+gayathrikk@users.noreply.github.com>
2024-06-20 12:57:52 +0530

    Atlas_Geometry_Dot.java

--- src/test/java/Sanity/Atlas/Atlas_Geometry_Dot.java
+++ src/test/java/Sanity/Atlas/Atlas_Geometry_Dot.java
@@ -26,7 +26,7 @@ public class Atlas_Geometry_Dot {
 		{
 			
 			  DesiredCapabilities dc = DesiredCapabilities.chrome();
-		        URL url = new URL("http://172.12.20.99:4444/wd/hub");
+		        URL url = new URL("http://172.20.23.92:4444/wd/hub");
 		        driver = new RemoteWebDriver(url, dc);
 		
 		       
